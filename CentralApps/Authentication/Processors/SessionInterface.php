5808c6dabc6601710922d645a00bd577cfdfde43
Andrew Tait <andrew.tait@bdaily.co.uk>
2013-02-08 14:49:04 +0000

    Formatting fixes

--- CentralApps/Authentication/Processors/SessionInterface.php
+++ CentralApps/Authentication/Processors/SessionInterface.php
@@ -1,12 +1,12 @@
 <?php
 namespace CentralApps\Authentication\Processors;
 
-interface SessionInterface {
-	
+interface SessionInterface
+{
 	public function __construct($session_name=null);
 	public function checkForAuthenticationSession();
 	public function getUserId();
 	public function setSessionValue($session_value);
 	public function logout();
-	
+
 }
