b3d888aaada7c6e789b684f9de0b164f6969deac
Michael Peacock <mkpeacock@gmail.com>
2013-03-03 19:07:41 -0500

    More tests

--- unit/CentralApps/Authentication/ProcessorTest.php
+++ unit/CentralApps/Authentication/ProcessorTest.php
@@ -21,7 +21,111 @@ class ProcessorTest extends \PHPUnit_Framework_TestCase
 	
 	public function testCheckForAuthentication()
 	{
-		$this->_processor->checkForAuthentication(true);
+		//$this->_processor->checkForAuthentication(true);
 		//$this->assertTrue(true);
 	}
+	
+	/**
+	 * @covers \CentralApps\Authentication\Processor::checkForAuthentication
+	 * @covers \CentralApps\Authentication\Processor::authenticateFromUserId
+	 */
+	public function testLoggingInWithSession()
+	{
+		$session_processor = $this->getMock('\CentralApps\Authentication\Processors\SessionInterface');
+		$session_processor->expects($this->once())
+						  ->method('checkForAuthenticationSession')
+						  ->will($this->returnValue(true));
+		$session_processor->expects($this->once())
+						  ->method('getUserId')
+						  ->will($this->returnValue(1));
+		$settings_provider = $this->getMock('\CentralApps\Authentication\SettingsProviderInterface');
+		$settings_provider->expects($this->once())
+						  ->method('getSessionProcessor')
+						  ->will($this->returnValue($session_processor));
+						  
+		$user = new \stdClass;
+		$user->userId = 1;
+		
+		$user_factory = $this->getMock('\CentralApps\Authentication\UserFactoryInterface');
+		$user_factory->expects($this->once())
+					 ->method('getUserByUserId')
+					 ->with($this->equalTo(1))
+					 ->will($this->returnValue($user));
+					 
+		$settings_provider->expects($this->once())
+						  ->method('getUserFactory')
+						  ->will($this->returnValue($user_factory));
+		
+		$processor = new Processor($settings_provider);
+		$processor->checkForAuthentication(false);			 
+		$this->assertTrue($processor->hasAttemptedToLogin(), "It doesn't look like we tried to login the user");	
+		$this->assertEquals($user, $processor->getUser());		 
+		
+	}
+	
+	public function testHasAttemptedToLogin()
+	{
+		$class = new \ReflectionClass("\CentralApps\Authentication\Processor");
+        $property = $class->getProperty('loginAttempted');
+        $property->setAccessible(true);
+        $property->setValue($this->_processor, true);
+		$this->assertTrue($this->_processor->hasAttemptedToLogin());
+		$property->setValue($this->_processor, false);
+		$this->assertFalse($this->_processor->hasAttemptedToLogin());	
+	}
+	
+	public function testLoginWithUsernameAndPassword()
+	{
+		$user_field = 'username';
+		$user_val = 'michael';
+		$pass_field = 'password';
+		$pass_val = 'test_password';
+		$post_data = array($user_field => $user_val, $pass_field => $pass_val);
+		$settings = $this->getMock('\CentralApps\Authentication\SettingsProviderInterface');
+		$settings->expects($this->once())
+				 ->method('getUsernameField')
+				 ->will($this->returnValue($user_field));
+		$settings->expects($this->once())
+				 ->method('getPasswordField')
+				 ->will($this->returnValue($pass_field));
+				 
+				 
+		$user = new \stdClass;
+		$user->userId = 1;
+		$user_factory = $this->getMock('\CentralApps\Authentication\UserFactoryInterface');		 
+		$user_factory->expects($this->once())
+					 ->method('getUserFromUsernameAndPassword')
+					 ->with($this->equalTo($user_val), $this->equalTo($pass_val))
+					 ->will($this->returnValue($user));
+		
+		
+		$settings->expects($this->once())
+				 ->method('getUserFactory')
+				 ->will($this->returnValue($user_factory));
+		
+		$session = $this->getMock('\CentralApps\Authentication\Processors\SessionInterface');
+		$session->expects($this->once())
+				->method('setSessionValue')
+				->with($this->equalTo(1));
+		
+		$settings->expects($this->once())
+				 ->method('getSessionProcessor')
+				 ->will($this->returnValue($session));
+				 
+		$user_gateway = $this->getMock('\CentralApps\Authentication\UserGateway');
+		$user_gateway->expects($this->once())	
+					 ->method('getUserId')
+					 ->will($this->returnValue(1));
+		
+		$settings->expects($this->once())
+				 ->method('getUserGateway')
+				 ->will($this->returnValue($user_gateway));	 		
+				 
+		$processor = new Processor($settings, $post_data);
+		$processor->checkForAuthentication();
+		$this->assertTrue($processor->hasAttemptedToLogin(), "It doesn't look like we tried to login the user");	
+		$this->assertEquals($user, $processor->getUser());
+		
+	}
+	
 }
